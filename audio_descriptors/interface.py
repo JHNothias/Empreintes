8e276f1ccb3962721b3a08d245336f872e4ce36e
JHNothias <jhnothias@gmail.com>
2026-05-18 17:32:25 +0200

    started to work on the interface for all descriptor computations

--- audio_descriptors/interface.py
+++ audio_descriptors/interface.py
@@ -1,1 +1,20 @@
 from LazyTree import LazyTree
+from Custom.interface import misc_interface
+from FluCoMa.interface import flucoma_interface
+
+def with_settings(tree : LazyTree, settings_location : str):
+    res = tree.new()
+    tree.se
+
+descriptors_interface = LazyTree({
+    "raw" : {
+        "misc" : misc_interface.new(),
+        "flucoma" : flucoma_interface.new()
+    },
+    "derived" : {
+
+    },
+    "settings" : {
+        "misc"
+    }
+})
