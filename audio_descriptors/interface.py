5e6213a5c381c82186277389149372c0e0008091
JHNothias <jhnothias@gmail.com>
2026-05-21 18:29:11 +0200

    Continued extending the interface for audio descriptors.

--- audio_descriptors/interface.py
+++ audio_descriptors/interface.py
@@ -2,19 +2,27 @@ from LazyTree import LazyTree
 from Custom.interface import misc_interface
 from FluCoMa.interface import flucoma_interface
 
-def with_settings(tree : LazyTree, settings_location : str):
-    res = tree.new()
-    tree.se
+def interface(settings):
+    descriptors_interface = LazyTree({
+        "inputs" : {
+            "slices" : None,
+        },
+        "raw" : {
+            "misc" : misc_interface.new(),
+            "flucoma" : flucoma_interface.new() if any(['flucoma' in descriptors_interface.get('requested_descriptors')])
+        },
+        "derived" : {
+            ...
+            for lib in descriptors_interface.spec["raw"]
+            for desc in descriptors_interface.spec["raw"][lib]["descriptors"]
+        },
+        "requested" : {
+            "descriptors" : lambda tree, i : [], # 'flucoma-centroid'
+            "transforms" : lambda tree, i : []
+        },
+        "settings" : {
+            "transforms" :
+                {}
+        }
 
-descriptors_interface = LazyTree({
-    "raw" : {
-        "misc" : misc_interface.new(),
-        "flucoma" : flucoma_interface.new()
-    },
-    "derived" : {
-
-    },
-    "settings" : {
-        "misc"
-    }
-})
+    })
